e71fba5ef71eb5d909a9cedc26baf40d3f256acf
Brandon De Leon <brandondeleon.615@gmail.com>
2021-08-16 13:28:48 -0500

    added export to buttons the clown module

--- src/scripts/ButtonsTheClown.js
+++ src/scripts/ButtonsTheClown.js
@@ -1,7 +1,17 @@
-// import {reservations}
+ import {reservations} from "./Reservations.js"
 
 
 
 export const buttonsTheClown = ()=>{
 
+    return `
+        <h1>Button's The Clown</h1>
+        <section class="serviceForm">
+        </section>
+
+        <section class="serviceRequests">
+            <h2>Reservation</h2>
+           
+        </section>
+    `
 }
